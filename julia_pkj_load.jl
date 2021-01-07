c17b93f3ed7e43a30149f7ea9e20bf7d8d65c7ef
VLonghand <2001vdvd@gmail.com>
2021-01-07 00:33:21 -0500

    a file to add all packages in one sweeping motion

---
+++ julia_pkj_load.jl
@@ -0,0 +1,10 @@
+begin
+    using Pkg
+    Pkg.add(["JLD","CSV", "DataFrames","Plots","PlotThemes"])
+    #using JLD
+    using CSV
+    using DataFrames
+    using Plots
+    using PlotThemes
+
+end
