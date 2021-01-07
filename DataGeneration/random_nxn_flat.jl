51a07fc248c61ce6477bb4823770fe59d850ccd2
VLonghand <2001vdvd@gmail.com>
2021-01-06 23:35:50 -0500

    rand nxn changeg, now faster, but need to add lables to data frame

--- DataGeneration/random_nxn_flat.jl
+++ DataGeneration/random_nxn_flat.jl
@@ -12,11 +12,35 @@ using DataFrames
 if "rand_flat.csv" in readdir("Data")
     rm("Data/rand_flat.csv")
 end
+# add names
+
+# g = [i*j for i = 1, j=1:1:n*n]
+# lables = convert(Vector{Any}, [i*j for i = 1, j=1:1:n*n])
+# f = DataFrame(lables)
+
+# push!(f, lables)
+
+# f |> CSV.write("Data/rand_flat.csv", append = true)
+
 
 # appending vectors to a CSV; check it's more efficient to save a list of vectors instead 
-for i in 1:N
-    local flat = rand(-1:2:1, (1, n*n))
-    df = DataFrame(flat)
-    df |> CSV.write("Data/rand_flat.csv", append = true)
+function saveSlow()
+    for i in 1:N
+        local flat = rand(-1:2:1, (1, n*n))
+        local df = DataFrame(flat)
+        df |> CSV.write("Data/rand_flat.csv", append = true)
+    end
 end
 
+# This is much faster
+function saveAfter()
+    local df = DataFrame(rand(-1:2:1, (1, n*n)))
+    for i in 1:(N-1)
+        local flat = rand(-1:2:1, (1, n*n))
+        push!(df, flat)
+    end
+    df |> CSV.write("Data/rand_flat.csv")
+    df = nothing
+end
+
+saveAfter()
