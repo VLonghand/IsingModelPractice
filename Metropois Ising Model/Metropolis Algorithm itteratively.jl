4bde576a762ec3a6a93bd7c3d52a1cf775aa24ef
fedot <2001vdvd@gmail.com>
2021-02-17 00:08:30 +0100

    Woring itterative Monti Carlo 4x4 Ising model simulation via Metropolis algorithm

--- Metropois Ising Model/Metropolis Algorithm itteratively.jl
+++ Metropois Ising Model/Metropolis Algorithm itteratively.jl
@@ -1,2 +1,42 @@
 using Colors, Plots, Images, Statistics, StaticArrays
 include("Funs.jl")
+
+σ_00 = [-1 1 -1 1; 1 -1 1 -1; -1 1 -1 1; 1 -1 1 -1]
+σ_trial = similar(σ_00)
+
+Ms = []
+Ts = []
+Mmeans = []
+
+function algotithm(T)
+    for i in 1:1_000_00
+        append!(Ms, M(σ_00))
+
+        global σ_trial = copy(σ_00)
+        global σ_trial[rand(1:16)] *= -1
+
+        r = rand()
+
+        if r <= w(T, σ_trial, σ_00)
+            global σ_00 = copy(σ_trial) 
+        end    
+    end
+end
+
+function itter_over_Temps()
+    for T in 0:0.01:3
+        algotithm(T)
+    
+        push!(Mmeans, abs(mean(Ms)))
+        push!(Ts, T)
+        global Ms = []
+    end
+
+end
+
+itter_over_Temps()
+
+plot(Ts,Mmeans, label=M)
+xlabel!("Temperature")
+ylabel!("Magnetization")
+title!("Magnetization vs Temperature")
