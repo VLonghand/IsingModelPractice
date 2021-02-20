1df9beb91091d3593e5c01709289b0834dfb0587
fedot <2001vdvd@gmail.com>
2021-02-20 01:12:59 +0100

    added devcontainer

--- Metropois Ising Model/Metropolis Algorithm itteratively.jl
+++ Metropois Ising Model/Metropolis Algorithm itteratively.jl
@@ -1,7 +1,10 @@
 using Colors, Plots, Images, Statistics, StaticArrays
 include("Funs.jl")
 
-σ_00 = [-1 1 -1 1; 1 -1 1 -1; -1 1 -1 1; 1 -1 1 -1]
+σ_00 = [-1 1 -1 1
+        1 -1 1 -1
+        -1 1 -1 1
+        1 -1 1 -1]
 σ_trial = similar(σ_00)
 
 Ms = []
@@ -9,8 +12,8 @@ Ts = []
 Mmeans = []
 
 function algotithm(T)
-    for i in 1:1_000_00
-        append!(Ms, M(σ_00))
+    for i in 1:1_000_000
+        push!(Ms, sum(σ_00))
 
         global σ_trial = copy(σ_00)
         global σ_trial[rand(1:16)] *= -1
@@ -18,7 +21,7 @@ function algotithm(T)
         r = rand()
 
         if r <= w(T, σ_trial, σ_00)
-            global σ_00 = copy(σ_trial) 
+            global σ_00 = σ_trial
         end    
     end
 end
@@ -27,7 +30,7 @@ function itter_over_Temps()
     for T in 0:0.01:3
         algotithm(T)
     
-        push!(Mmeans, abs(mean(Ms)))
+        push!(Mmeans, abs(mean(Ms)/16)) # /16 to 'normalize' or average magnetization over all spins
         push!(Ts, T)
         global Ms = []
     end
@@ -36,7 +39,7 @@ end
 
 itter_over_Temps()
 
-plot(Ts,Mmeans, label=M)
+plot(Ts, Mmeans, label=M)
 xlabel!("Temperature")
 ylabel!("Magnetization")
 title!("Magnetization vs Temperature")
