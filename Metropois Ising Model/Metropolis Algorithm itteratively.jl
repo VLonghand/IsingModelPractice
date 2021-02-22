cb0c045aa9f642058792e70515f7c5b97474e4d9
fedot <2001vdvd@gmail.com>
2021-02-22 05:32:41 +0100

    Changed to use multiprocessing

--- Metropois Ising Model/Metropolis Algorithm itteratively.jl
+++ Metropois Ising Model/Metropolis Algorithm itteratively.jl
@@ -1,17 +1,47 @@
-using Colors, Plots, Images, Statistics, StaticArrays
-include("Funs.jl")
+@everywhere using Plots, Statistics
+#include("FunsForSquareInsingModel.jl")
+using Distributed
 
-σ_00 = [-1 1 -1 1
+@everywhere function H(spin_grid)
+    acc = 0
+    n = 4
+    J = 1
+    #spin_grid = reshape(spin_vec, (n,n))
+
+
+    for i = 1:n, j = 1:n
+        local i_1 = i-1
+        local j_1 = j-1
+        if i == 1
+            i_1 = n
+        end
+        if j == 1
+            j_1 = n
+        end
+        acc += spin_grid[i,j]*spin_grid[i_1,j]*(-J)
+        acc += spin_grid[i,j]*spin_grid[i,j_1]*(-J)
+    end
+    return acc
+end
+@everywhere function M(spin_grid)
+    return sum(spin_grid)
+end
+@everywhere function w(T, σ_trial, σ_0)
+    β = 1/T
+    return exp(-β*(H(σ_trial) - H(σ_0)))
+end
+
+@everywhere σ_00 = [-1 1 -1 1
         1 -1 1 -1
         -1 1 -1 1
         1 -1 1 -1]
-σ_trial = similar(σ_00)
+@everywhere  σ_trial = similar(σ_00)
 
-Ms = []
-Ts = []
-Mmeans = []
+@everywhere Ms = []
+@everywhere Ts = []
+@everywhere Mmeans = []
 
-function algotithm(T)
+@everywhere function algotithm(T)
     for i in 1:1_000_000
         push!(Ms, sum(σ_00))
 
@@ -26,7 +56,7 @@ function algotithm(T)
     end
 end
 
-function itter_over_Temps()
+@everywhere function itter_over_Temps()
     for T in 0:0.01:3
         algotithm(T)
     
@@ -37,7 +67,15 @@ function itter_over_Temps()
 
 end
 
-itter_over_Temps()
+addprocs(7)
+print(nprocs())
+print(nworkers())
+
+for i in workers()
+    r = remotecall_fetch(itter_over_Temps, WorkerPool(workers()))
+end
+
+# itter_over_Temps()
 
 plot(Ts, Mmeans, label=M)
 xlabel!("Temperature")
