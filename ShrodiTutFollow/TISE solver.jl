f3db0ac81e763e8cdd3adce62e9c961ebb6eb365
VLonghand <V>
2021-04-26 18:23:41 -0400

    now even more include friendly and should use less memory, requires pre-allocation

--- ShrodiTutFollow/TISE solver.jl
+++ ShrodiTutFollow/TISE solver.jl
@@ -4,34 +4,35 @@ using Arpack
 # This should be include friendly
 
 # Simple harmonic oscilator to test slover 
-function SHO(L, kx,ky,cx=0,cy=0)
-    xy = range(-20,20,length=L)
+function SHO(grid1, xy, L, kx, ky;cx=0,cy=0)
+    
 
     # TODO: find a way to make this more efficient 
-    V = ones(L,L)
+    
     for i in 1:L
         for j in 1:L
-        V[j,i] = 0.5 * (kx*(xy[i]-cx)^2 + ky*(xy[j]-cy)^2)
+        grid1[j,i] = 0.5 * (kx*(xy[i]-cx)^2 + ky*(xy[j]-cy)^2)
         end
     end
-    return V
+
+    return grid1
 end
 
-# V = SHO(L,2,1,0,0)
+# V = SHO(ones(256,256), collect(range(-20,20,length=256)) , 256,2,1)
 # Plots.heatmap(V, c=:amp)
 
-begin
-    # Analysical vs finite difference
-    L = 256
-    limit=20
-    x=range(-limit, limit, length=L)
-    y=copy(x)
-    dx = x[2]-x[1]
-    dy = y[2]-y[1]
+# begin
+#     # Analysical vs finite difference
+#     L = 256
+#     limit=20
+#     x=range(-limit, limit, length=L)
+#     y=copy(x)
+#     dx = x[2]-x[1]
+#     dy = y[2]-y[1]
 
-    # constructing second derivative matrix
-    dia_len = sqrt(L^2^2*2)
-end
+#     # constructing second derivative matrix
+#     dia_len = sqrt(L^2^2*2)
+# end
 
 
 # (-∇²+V̂)ψ = εψ
@@ -40,13 +41,14 @@ end
 begin
     # di = (2*dx*dy) 
     # this constructs the enire second derivative operator 
-    T̂ = spdiagm(0=>[4 for i in 1:L^2], 1=>[-1 for i in 1:(L^2-1)],
-                -1=>[-1 for i in 1:(L^2-1)], L=>[-1 for i in 1:(L-1)^2],
-                -L=>[-1 for i in 1:(L-1)^2])
+    # T̂ = (spdiagm(0=>[4 for i in 1:L^2], 1=>[-1 for i in 1:(L^2-1)],
+    #             -1=>[-1 for i in 1:(L^2-1)], L=>[-1 for i in 1:(L-1)^2],
+    #             -L=>[-1 for i in 1:(L-1)^2]))/(2*dx*dy)
+
 
     # UnicodePlots.spy(T̂) # quick way to check the diagonals
 
-    T̂ = T̂/(2*dx*dy)
+
 end
 
 # julia is does column major everything 
@@ -60,35 +62,50 @@ function row_major_flatten(mop::Matrix)
     return vesc
 end
 
+function second_der_oper( L, 𝑑) #assuming square grid, i.e. dx = dy = 𝑑
+    dx = 𝑑
+    dy = 𝑑
+    T̂ = (spdiagm(0=>[4 for i in 1:L^2], 1=>[-1 for i in 1:(L^2-1)],
+                -1=>[-1 for i in 1:(L^2-1)], L=>[-1 for i in 1:(L-1)^2],
+                -L=>[-1 for i in 1:(L-1)^2]))/(2*dx*dy)    
 
-# T̂ = convert(SparseMatrixCSC{Float32,Int64},T̂)
-function solve1(potential)
-    V = spdiagm(0=>potential[:])
+    return T̂
+end
+
+
+function solve1(potential, T̂)
+    V̂ = spdiagm(0=>potential[:])
     # V is an Array{Any,2}, I don't know why {Any}
-    V = convert(SparseMatrixCSC{Float32,Int64},V) # without this eigs spits arror about {Any}
+    V̂ = convert(SparseMatrixCSC{Float32,Int64},V̂) # without this eigs spits arror about {Any}
 
-    H = V + T̂
+    Ĥ = V̂ + T̂
 
     # TODO: 𝜓 is a waste of memory
-    𝜆, 𝜓 = eigs(H, nev=1, which=:SM) # nev=1 cause we don't need others
+    𝜆, 𝜓 = eigs(Ĥ, nev=1, which=:SM) # nev=1 cause we don't need others
     return 𝜆, 𝜓
 end
 
 
 # Testing solver on SHO
 function test_solver_w_SHO()
-
+    grid1 = ones(256,256)
+    xy = collect(range(-20,20,length=256))
+    L = 256
     num = 10
+    limit = 20
+    x⚥=range(-limit, limit, length=L)      #apparently 40/256 far enough away from actual Δ to cause major error
+    T̂ = second_der_oper(L, x⚥[1]-x⚥[2])
+
     kx = rand(num)*0.16
     ky = rand(num)*0.16
     cx = (rand(num).-0.5).*16
     cy = (rand(num).-0.5).*16
 
     for i in 1:num
-        𝜆, 𝜓 = solve1(SHO(L, kx[i],ky[i],cy[i],cx[i]))
+        𝜆, 𝜓 = solve1(SHO(grid1, xy, L, kx[i],ky[i],cx = cx[i],cy = cy[i]), T̂)
         # print(𝜆)
-        numerical = round(real(𝜆[1]), digits=5)
-        analytical = round(0.5 * (sqrt(kx[i]) + sqrt(ky[i])), digits=5)
+        numerical = round(real(𝜆[1]), digits=8)
+        analytical = round(0.5 * (sqrt(kx[i]) + sqrt(ky[i])), digits=8)
         error = round(100*abs(numerical-analytical)/analytical, digits=5)
         print("Numerical: $numerical,  Analysical: $analytical, Error: $error% \n")
     end 
